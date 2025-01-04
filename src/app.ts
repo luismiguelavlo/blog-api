63eef98ef09bd5ebd6819e7b5412bab99a1f98a6
Luis Miguel <redes.teleco.lm@gmail.com>
2025-01-03 23:13:51 -0500

    next commit

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
+import { envs } from "./config";
+import { PostgresDatabase } from "./data";
 import { AppRoutes } from "./presentation/routes";
 import { Server } from "./presentation/server";
 
 async function main() {
+  const postgres = new PostgresDatabase({
+    username: envs.DB_USERNAME,
+    password: envs.DB_PASSWORD,
+    host: envs.DB_HOST,
+    database: envs.DB_DATABASE,
+    port: envs.DB_PORT,
+  });
+
+  await postgres.connect();
+
   const server = new Server({
-    port: 3000,
+    port: envs.PORT,
     routes: AppRoutes.routes,
   });
 
